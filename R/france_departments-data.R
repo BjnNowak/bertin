0b902f8aa12c838b636e2c11d8e436212e39bba3
BjnNowak <bjn.nowak@gmail.com>
2023-05-22 11:26:54 +0200

    add dataset at department scale

---
+++ R/france_departments-data.R
@@ -0,0 +1,11 @@
+#' France departments population
+#'
+#' Population density for administrative departments of France.
+#' the population density is expressed in number of inhabitants per square kilometer.
+#'
+#' @docType data
+#'
+#' @usage data(france_departments)
+#'
+#' @format An object of class sf.
+"france_departments"
