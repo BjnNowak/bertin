f954bc82f01ad503b3941b35b52895a899b96109
BjnNowak <bjn.nowak@gmail.com>
2023-05-22 09:40:15 +0200

    supress fr manual

--- R/france_regions.R
+++
@@ -1,9 +0,0 @@
-#' France regions population
-#'
-#' Population density for administrative regions of France
-#'
-#' @docType data
-#'
-#' @usage data(france_regions)
-#'
-#' @format An object of class sf.
